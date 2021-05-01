69b59434e75e7d031daaf75f802faea21783f8fd
Sole Roa <msoleroa@gmail.com>
2021-04-30 22:07:47 -0300

    first commit

--- src/guia05/main.java
+++ src/guia05/main.java
@@ -2,13 +2,7 @@ package guia05;
 
 public class main {
 	 public static void main(String[] args) {
-			   //Usuario u = new Usuario("sole", "sole@gmail.com",);
-			   //Alquiler a = new Alquiler();
-		       //Herramienta h = new Herramienta();
-		 		//Creo un nuevo oficio 
-			   Oficio o = new Oficio("carpinteria");
-			   //Creo un nuevo trabajador 
-			   Trabajador t = new Trabajador("ale",o,"ale@gmail.com",0.5, 500.0);
+		
 			   
 	 }
 }
